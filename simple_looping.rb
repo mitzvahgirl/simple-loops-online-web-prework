b275ab564e008655fc8d656a09c6d841b353e491
Amy Fogelman <abfnj1@gmail.com>
2019-05-27 00:49:23 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -15,6 +15,7 @@ end
 
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
+  5.times do
   puts "Welcome to Flatiron School's Web Development Course!"
   
 end
